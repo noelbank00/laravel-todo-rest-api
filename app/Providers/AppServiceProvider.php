044d3b779df9e682412b4298eee10a12d9a9a492
Noel <noel.bank@insource.hu>
2022-08-08 14:11:54 +0200

    Bind interface

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Http\Interfaces\TodoInterface;
+use App\Http\Services\TodoService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(TodoInterface::class, TodoService::class);
     }
 
     /**
