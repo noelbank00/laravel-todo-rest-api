675766eaa82c0585dda541df49dfdee59c50b44b
Noel <noel.bank@insource.hu>
2022-08-08 14:11:43 +0200

    resource fix

--- app/Http/Requests/StoreTodoRequest.php
+++ app/Http/Requests/StoreTodoRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateTodoItemRequest extends FormRequest
+class StoreTodoRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -13,7 +13,7 @@ class UpdateTodoItemRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class UpdateTodoItemRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required|string|max:80',
+            'description' => 'string|nullable|max:750',
         ];
     }
 }
--- database/migrations/2022_08_08_095337_create_todo_table.php
+++ database/migrations/2022_08_08_095337_create_todo_table.php
@@ -12,7 +12,7 @@ return new class extends Migration {
      */
     public function up()
     {
-        Schema::create('todo_items', function (Blueprint $table) {
+        Schema::create('todos', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->longText('description')->nullable();
